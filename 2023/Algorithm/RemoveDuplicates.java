c756aac830d31059b1e3ad3061a36d907d285cbd
siwonlim <dbu7422@gmail.com>
2024-01-10 23:53:58 -0800

    20230110

---
+++ 2023/Algorithm/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package Algorithm;
+
+public class RemoveDuplicates {
+    static public void main(String[] args) throws Exception{
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        removeDuplicates(nums);
+    }
+    static public int removeDuplicates(int[] nums) {
+        int index = 1;
+        for(int i = 1 ; i < nums.length ; i++){
+            if(nums[i-1] != nums[i]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
