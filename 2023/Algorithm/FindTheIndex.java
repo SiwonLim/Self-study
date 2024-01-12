d101c46d275ed6422098a820ce9aa2ee309dcac8
siwonlim <dbu7422@gmail.com>
2024-01-11 20:50:46 -0800

    20230111

---
+++ 2023/Algorithm/FindTheIndex.java
@@ -0,0 +1,12 @@
+package Algorithm;
+
+public class FindTheIndex {
+    static public void main(String[] args) throws Exception{
+        strStr("sasdbutsasdsad","sad");
+
+    }
+    static public int strStr(String haystack, String needle) {
+        int pivot = haystack.indexOf(needle, 0);
+        return pivot;
+    }
+}
